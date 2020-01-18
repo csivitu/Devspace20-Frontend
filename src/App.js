0a85ab108c2be810067b8025755ea4d49e11aded
thebongy <rishit.bansal0@gmail.com>
2020-01-18 19:36:08 +0530

    feat: Devspace features section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,82 @@
 import React from 'react';
+import Section from './components/section';
+import { Container, Row, Col } from 'react-bootstrap';
 
+
+
+function generateDevspaceFeatures() {
+    const devspaceContent = [
+        {
+            heading: "Reinvening Experience",
+            content: "An investment in knowledge earns the highest dividends; come have a hands-on session in workshops that equip you for the future."
+        },
+        {
+            heading: "Inspiring Keynotes",
+            content: "An investment in knowledge earns the highest dividends; come have a hands-on session in workshops that equip you for the future."
+        },
+        {
+            heading: "Learning Sessions",
+            content: "An investment in knowledge earns the highest dividends; come have a hands-on session in workshops that equip you for the future."
+        },
+        {
+            heading: "Hackathon",
+            content: "An investment in knowledge earns the highest dividends; come have a hands-on session in workshops that equip you for the future."
+        }
+    ];
+
+    function devspaceFeature(heading, content) {
+        return (
+            <div className="w-100 bg-dark p-4 my-4 mx-3" style={{
+                boxShadow: '0px 0px 18px #00000066'
+            }}>
+                <h1 className="text-primary text-uppercase" style={{
+                    minHeight: '40%'
+                }}>
+                    {heading}
+                </h1>
+                <p class="text-white">
+                    {content}
+                </p>
+            </div>
+        )
+    }
+
+    const features = [];
+
+    for (const content of devspaceContent) {
+        features.push((
+            <Col md={5} className="d-flex justify-content-center">
+                {devspaceFeature(content.heading, content.content)}
+            </Col>
+        ));
+    }
+
+    return (
+        <Row className="justify-content-center">
+            {features}
+        </Row>
+    );
+
+}
 class App extends React.Component {
-  componentDidMount() {
-  }
-  render() {
-      return null;
-  }
+    componentDidMount() {
+    }
+    render() {
+
+        return (
+            <div>
+                {Section({
+                    headingText: 'DEVSPACE',
+                    content: (
+                        <Container className="pt-3">
+                            {generateDevspaceFeatures()}
+                        </Container>
+                    ),
+                    headingAlignment: 'right'
+                })}
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ src/components/section.js
@@ -0,0 +1,40 @@
+
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import classnames from 'classnames';
+
+export default function Section(props) {
+    const contentOrder = {
+        heading: 1,
+        content: 2
+    }
+    if (props.headingAlignment && props.headingAlignment === 'right') {
+        contentOrder.heading = 2;
+        contentOrder.content = 1;
+    } else {
+        props.headingAlignment = 'left';
+    }
+    return (
+        <div className="section">
+            <div
+            style = {{
+                order: contentOrder.heading,
+
+            }} 
+            className={classnames(
+                "section-heading",
+                "section-heading-blue",
+                "section-heading-" + props.headingAlignment)
+                }>
+                <div>{props.headingText}</div>
+            </div>
+            <div
+            style = {{
+                order: contentOrder.content
+            }} 
+            className="section-content">
+                {props.content}
+            </div>
+        </div>
+    );
+}
